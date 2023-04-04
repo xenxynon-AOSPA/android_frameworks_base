28f52bf465120d940fcde85e37f0724eefb87df6
Shamik <shamikjoshi471@gmail.com>
2023-04-04 18:31:59 +0200

    Revert "SystemUI: Fix QS clock overlapping on UI mode change"
    This reverts commit 4c664856936fc6a975cfdd261961146c312fdc1b.

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -201,7 +201,7 @@ public class QuickStatusBarHeader extends FrameLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         if (mDatePrivacyView.getMeasuredHeight() != mTopViewMeasureHeight) {
             mTopViewMeasureHeight = mDatePrivacyView.getMeasuredHeight();
-            updateAnimators();
+            post(this::updateAnimators);
         }
     }
 
